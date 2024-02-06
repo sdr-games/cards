095ac7f8c6a5b2e600466796e344f6c7d14f2ef6
stanislav-sobko <13215243+StanislavSobko@users.noreply.github.com>
2024-02-06 21:29:05 +0300

    added level const

--- Assets/Modules/CharacterModule/Scripts/Models/PlayerCharacterParamsModel.cs
+++ Assets/Modules/CharacterModule/Scripts/Models/PlayerCharacterParamsModel.cs
@@ -8,6 +8,7 @@ namespace SDRGames.Whist.CharacterModule.Models
     public class PlayerCharacterParamsModel : CommonCharacterParamsModel
     {
         private readonly string CHARACTER_NAME = "Валиор";
+        private const int DEFAULT_BASE_LEVEL = 1;
 
         public event EventHandler<LevelChangedEventArgs> LevelChanged;
         public event EventHandler<MagicDamageMultiplierChangedEventArgs> MagicDamageMultiplierChanged;
@@ -15,6 +16,7 @@ namespace SDRGames.Whist.CharacterModule.Models
         public PlayerCharacterParamsModel(string characterName, int level, Points healthPoints, Points staminaPoints, Points breathPoints, Points physicalArmor, Points magicShield, Dice physicalDamage, float magicDamageMultiplier) : base(characterName, level, healthPoints, staminaPoints, breathPoints, physicalArmor, magicShield, physicalDamage, magicDamageMultiplier)
         {
             CharacterName = CHARACTER_NAME;
+            Level = DEFAULT_BASE_LEVEL;
         }
     }
 }
