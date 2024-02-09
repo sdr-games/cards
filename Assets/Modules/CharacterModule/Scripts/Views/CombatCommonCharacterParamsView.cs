5317fa5579ce6130569170da8101147ecaf8763c
stanislav-sobko <13215243+StanislavSobko@users.noreply.github.com>
2024-02-09 20:48:06 +0300

    added combat view

---
+++ Assets/Modules/CharacterModule/Scripts/Views/CombatCommonCharacterParamsView.cs
@@ -0,0 +1,42 @@
+using SDRGames.Islands.PointsModule.Views;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace SDRGames.Whist.CharacterModule
+{
+    public class CombatCommonCharacterParamsView : MonoBehaviour
+    {
+        [field: SerializeField] public PointsBarView ArmorPointsBarView { get; protected set; }
+        [field: SerializeField] public PointsBarView BarrierPointsBarView { get; protected set; }
+        [field: SerializeField] public PointsBarView HealthPointsBarView { get; protected set; }
+
+        private void OnEnable()
+        {
+            if (HealthPointsBarView == null)
+            {
+                Debug.LogError("Health PointsBar View не был назначен");
+                #if UNITY_EDITOR
+                    EditorApplication.isPlaying = false;
+                #endif
+            }
+
+            if (ArmorPointsBarView == null)
+            {
+                Debug.LogError("Armor PointsBar View не был назначен");
+                #if UNITY_EDITOR
+                    EditorApplication.isPlaying = false;
+                #endif
+            }
+
+            if (BarrierPointsBarView == null)
+            {
+                Debug.LogError("Barrier PointsBar View не был назначен");
+                #if UNITY_EDITOR
+                    EditorApplication.isPlaying = false;
+                #endif
+            }
+        }
+    }
+}
