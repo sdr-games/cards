f6dfa4d4f100c6c72d18c74d25e6a968abe4b964
stanislav-sobko <13215243+StanislavSobko@users.noreply.github.com>
2024-02-11 15:19:09 +0300

    added glory and experience

--- Assets/Modules/CharacterModule/Scripts/Models/PlayerCharacterParamsModel.cs
+++ Assets/Modules/CharacterModule/Scripts/Models/PlayerCharacterParamsModel.cs
@@ -11,13 +11,44 @@ namespace SDRGames.Whist.CharacterModule.Models
         private readonly string CHARACTER_NAME = "Валиор";
         private const int DEFAULT_BASE_LEVEL = 1;
 
+        public int Experience { get; private set; }
+        public int Glory { get; private set; }
+
         public event EventHandler<LevelChangedEventArgs> LevelChanged;
+        public event EventHandler<ExperienceChangedEventArgs> ExperienceChanged;
+        public event EventHandler<GloryChangedEventArgs> GloryChanged;
         public event EventHandler<MagicDamageMultiplierChangedEventArgs> MagicDamageMultiplierChanged;
 
-        public PlayerCharacterParamsModel(string characterName, int level, Points healthPoints, Points staminaPoints, Points breathPoints, Points physicalArmor, Points magicShield, Dice physicalDamage, float magicDamageMultiplier) : base(characterName, level, healthPoints, staminaPoints, breathPoints, physicalArmor, magicShield, physicalDamage, magicDamageMultiplier)
+        public PlayerCharacterParamsModel(string characterName, int level, Points healthPoints, Points staminaPoints, Points breathPoints, Points physicalArmor, Points magicShield, Dice physicalDamage, float magicDamageMultiplier, int experience, int glory) : base(characterName, level, healthPoints, staminaPoints, breathPoints, physicalArmor, magicShield, physicalDamage, magicDamageMultiplier)
         {
             CharacterName = CHARACTER_NAME;
             Level = DEFAULT_BASE_LEVEL;
+            Experience = experience;
+            Glory = glory;
+        }
+
+        public void IncreaseLevel(int level)
+        {
+            Level += level;
+            LevelChanged?.Invoke(this, new LevelChangedEventArgs(Level));
+        }
+
+        public void IncreaseExperience(int experience)
+        {
+            Experience += experience;
+            ExperienceChanged?.Invoke(this, new ExperienceChangedEventArgs(Experience));
+        }
+
+        public void IncreaseGlory(int glory)
+        {
+            Glory += glory;
+            GloryChanged?.Invoke(this, new GloryChangedEventArgs(Glory));
+        }
+
+        public void DecreaseGlory(int glory)
+        {
+            Glory -= glory;
+            GloryChanged?.Invoke(this, new GloryChangedEventArgs(Glory));
         }
     }
 }
