8eb796e71ed20403d25dd759126a859783a4c90b
stanislav-sobko <13215243+StanislavSobko@users.noreply.github.com>
2024-02-16 16:13:57 +0300

    added pinview, scene and prefab

---
+++ Assets/Modules/GlobalMapModule/Scripts/Views/EventArgs/PinClickedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SDRGames.Whist.GlobalMapModule.Views
+{
+    public class PinClickedEventArgs : EventArgs
+    {
+        public GlobalMapPinView.Status Status { get; private set; }
+
+        public PinClickedEventArgs(GlobalMapPinView.Status status)
+        {
+            Status = status;
+        }
+    }
+}
